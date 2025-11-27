6a33f5db1a9613c4e4815eec1e0181ea6c182be3
sommanat_wo <sommanat_wo@minor.com>
2025-11-27 12:07:50 +0700

    Update for env setup

--- Run_Setenv.bat
+++ Run_Setenv.bat
@@ -1,5 +1,14 @@
 @echo off
-echo Activate Virtual Environment...
-call "D:\AI_Invoice_Extract\env\Scripts\activate.bat"
-echo Activate Virtual Environment complete!
+
+echo Python Virtual Environment...
+python -m venv env
+echo Python Virtual Environment... Created.
+echo Activating Virtual Environment...
+call env\Scripts\activate.bat
+echo Virtual Environment Activated.
+echo Installing Required Packages...
+pip install -r requirements.txt
+echo Required Packages Installed.
+
+
 pause
